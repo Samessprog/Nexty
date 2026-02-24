a2dbf84eb93d14f10b09deccbe365a77864b689a
Sames <100785560+Samessprog@users.noreply.github.com>
2026-02-24 20:06:44 +0100

    Create a terrform under AWS cognito Added.dockergnore for a faster and more secure build and fixes for.gitignore (#11)

---
+++ terraform/modules/cognito/variables.tf
@@ -0,0 +1,9 @@
+variable "user_pool_name" {
+  type        = string
+  description = "Name of the Cognito User Pool"
+}
+
+variable "client_name" {
+  type        = string
+  description = "Name of the Cognito app client"
+}
